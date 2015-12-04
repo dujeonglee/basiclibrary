1f24e1d570b3b3b1b358671f05017d4d93d0e27f
Dujeong Lee <dujeong.lee82@gmail.com>
2015-12-04 19:41:53 +0900

    Update avltree_test.cpp

--- avltree/avltree_test.cpp
+++ avltree/avltree_test.cpp
@@ -1,18 +1,64 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 #include "avltree.h"
 
 int main(void){
     avltree<int, int> tree;
 
-    for(int i = 0 ; i < 10000000 ; i++){
-        int data = rand();
+    //srand(time(NULL));
+    for(int i = 0 ; i < 50 ; i++){
+        int data = rand()%100;
         if(tree.insert(data,data) == true){
             //printf("Add %d\n", data);
         }
     }
-    printf("size %u\n", tree.size());
-    //tree.print();
+/*
+    tree.delete_root();
+    tree.delete_root();
+    tree.delete_root();
+    tree.delete_root();
+    tree.delete_root();
+    tree.delete_root();
+    tree.delete_root();
+    tree.delete_root();
+    tree.delete_root();
+    tree.delete_root();
+    tree.delete_root();
+    tree.delete_root();
+    tree.delete_root();
+    tree.delete_root();
+    tree.delete_root();
+    tree.delete_root();
+    tree.delete_root();
+    tree.delete_root();
+    tree.delete_root();
+    tree.delete_root();
+    tree.delete_root();
+    tree.delete_root();
+    tree.delete_root();
+    tree.delete_root();
+    tree.delete_root();
+    tree.delete_root();
+    tree.delete_root();
+    tree.delete_root();
+    tree.delete_root();
+    tree.delete_root();
+    tree.delete_root();
+    tree.delete_root();
+    tree.delete_root();
+    tree.delete_root();
+    tree.delete_root();
+    tree.delete_root();
+    tree.delete_root();
+*/
+    tree.print();
+    tree.delete_root();
+    tree.print();
+printf("=========\n");
+    tree.insert(2,2);
+    tree.print();
+//    while(1);
     return 0;
 }

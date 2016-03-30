520d930018b42d15bb38c63473a91a1bfe6c7902
Dujeong Lee <dujeong.lee82@gmail.com>
2016-03-30 21:49:39 +0900

    Single shot timer class.

--- single_shot_timer/singleshottimer.h
+++ single_shot_timer/singleshottimer.h
@@ -3,32 +3,36 @@
 #include <thread>
 #include <mutex>
 
-typedef void (*timeout_handler)();
+typedef void (*handler_func)();
 
 class singleshottimer{
 private:
     std::timed_mutex _lock;
     unsigned int _timeout;
-    timeout_handler _handler;
+    handler_func _cancel_handler;
+    handler_func _timeout_handler;
     std::thread _thread;
 
 public:
     explicit singleshottimer(){};
     ~singleshottimer(){};
-    void start(unsigned int timeout_milles, timeout_handler handler){
+    void start(unsigned int timeout_milles, handler_func stop_handler, handler_func timeout_handler){
         _timeout = timeout_milles;
-        _handler = handler;
+        _cancel_handler = stop_handler;
+        _timeout_handler = timeout_handler;
         _thread = std::thread([&](){
             this->_lock.lock();
             if(this->_lock.try_lock_for(std::chrono::milliseconds(this->_timeout)) == false){
                 this->_lock.unlock();
-                this->_handler();
+                this->_timeout_handler();
+            }else{
+                this->_cancel_handler();
             }
             return;
         });
     }
 
-    void stop(){
+    void cancel(){
         _lock.unlock();
     }
 };

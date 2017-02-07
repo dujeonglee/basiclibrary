835d93f62954f05d5f701649a3e7c2cfe4f04f26
Dujeong Lee <dujeong.lee82@gmail.com>
2017-02-08 02:14:24 +0900

    example is updated.

--- threadpool/example.cpp
+++ threadpool/example.cpp
@@ -4,10 +4,22 @@
 
 int main ()
 {
-    ThreadPool pool(4);
-    pool.enqueue([](){std::cout<<"hey1\n";std::this_thread::sleep_for( std::chrono::seconds(3) );});
-    pool.enqueue([](){std::cout<<"hey2\n";std::this_thread::sleep_for( std::chrono::seconds(3) );});
-    pool.enqueue([](){std::cout<<"hey3\n";std::this_thread::sleep_for( std::chrono::seconds(3) );});
-    pool.enqueue([](){std::cout<<"hey4\n";std::this_thread::sleep_for( std::chrono::seconds(3) );});
+    ThreadPool<2, 2> pool;
+    std::thread* prio0;
+    std::thread* prio1;
+    prio0 = new std::thread([&](){
+        while(1)
+        {
+            pool.enqueue([&](){std::cout<<"!!!!!!!!!!!!!!!!!!!!!!!!PRI0\n";std::this_thread::sleep_for(std::chrono::seconds(1));}, 0);
+            std::this_thread::sleep_for(std::chrono::seconds(3));
+        }
+    });
+    prio1 = new std::thread([&](){
+        while(1)
+        {
+            pool.enqueue([&](){std::cout<<"PRI1\n";}, 1);
+        }
+    });
+    while(1);
     return 0;
 }

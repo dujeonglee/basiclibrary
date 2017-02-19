726442db70665c1e382091159bcd17bc94cd7bc4
Dujeong Lee <dujeong.lee82@gmail.com>
2017-02-19 13:08:59 +0900

    The number of timeout handler threads is tunnable as SingleShotTimer<10> timer. Destructor completely free allocated memory.

--- singleshottimer/main.cpp
+++ singleshottimer/main.cpp
@@ -2,22 +2,22 @@
 #include <iostream>
 #include <unistd.h>
 
-SingleShotTimer timer;
+SingleShotTimer<10> timer;
 int main()
 {
     while(1)
     {
-        timer.ScheduleTask(5000, []{std::cout<<"TO 5000\n";}, nullptr);
-        timer.ScheduleTask(1000, []{std::cout<<"TO 1000\n";}, nullptr);
-        timer.ScheduleTask(3000, []{std::cout<<"TO 3000\n";}, nullptr);
-        timer.ScheduleTask(4000, []{std::cout<<"TO 4000\n";}, nullptr);
-        timer.ScheduleTask(2000, []{std::cout<<"TO 2000\n";}, nullptr);
-        timer.ScheduleTask(2500, []{std::cout<<"TO 2500\n";}, nullptr);
-        timer.ScheduleTask(1500, []{std::cout<<"TO 1500\n";}, nullptr);
-        timer.ScheduleTask(3500, []{std::cout<<"TO 3500\n";}, nullptr);
-        timer.ScheduleTask(4500, []{std::cout<<"TO 4500\n";}, nullptr);
-        timer.ScheduleTask(5500, []{std::cout<<"TO 5500\n";}, nullptr);
-        std::this_thread::sleep_for (std::chrono::milliseconds(10000));
+        timer.ScheduleTask(5, []{std::cout<<"TO 5000\n";}, nullptr);
+        timer.ScheduleTask(1, []{std::cout<<"TO 1000\n";}, nullptr);
+        timer.ScheduleTask(3, []{std::cout<<"TO 3000\n";}, nullptr);
+        timer.ScheduleTask(4, []{std::cout<<"TO 4000\n";}, nullptr);
+        timer.ScheduleTask(2, []{std::cout<<"TO 2000\n";}, nullptr);
+        timer.ScheduleTask(2, []{std::cout<<"TO 2500\n";}, nullptr);
+        timer.ScheduleTask(1, []{std::cout<<"TO 1500\n";}, nullptr);
+        timer.ScheduleTask(3, []{std::cout<<"TO 3500\n";}, nullptr);
+        timer.ScheduleTask(4, []{std::cout<<"TO 4500\n";}, nullptr);
+        timer.ScheduleTask(5, []{std::cout<<"TO 5500\n";}, nullptr);
+        std::this_thread::sleep_for (std::chrono::milliseconds(1));
     }
     return 0;
 }

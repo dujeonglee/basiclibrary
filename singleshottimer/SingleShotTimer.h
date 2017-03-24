9cdff4649b291ab5a322c82b5f2de15eee36e247
Dujeong Lee <dujeong.lee82@gmail.com>
2017-03-24 22:43:34 +0900

    Busy and Non-busy waiting can be chosen with -DBUSYWAITING option

--- singleshottimer/SingleShotTimer.h
+++ singleshottimer/SingleShotTimer.h
@@ -2,28 +2,33 @@
 #define SINGLESHOTTIMER_H_
 #include "ThreadPool.h"
 #include <chrono>
+#include <ctime>
 
 typedef std::chrono::time_point<std::chrono::steady_clock> SSTTime;
 
 class TimerInfo
 {
 public:
-    std::atomic<bool> m_Active;
     SSTTime m_TargetTime;
     std::function <void()> m_TimeoutHandler;
-    std::function <void()> m_CancelHandler;
+    void PrintTime()
+    {
+        std::time_t ttp = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now() + (m_TargetTime - std::chrono::steady_clock::now()));
+        std::cout << "time: " << std::ctime(&ttp);
+    }
 };
 template <unsigned long CONCURRENCY>
 class SingleShotTimer
 {
 private:
-    std::mutex m_Lock;
+    std::mutex m_APILock;
+
+    std::mutex m_ActiveTimerInfoListLock;
     std::condition_variable m_Condition;
     std::vector<TimerInfo*> m_ActiveTimerInfoList;
     std::thread* m_Thread;
     ThreadPool<1, CONCURRENCY> m_ThreadPool;
     std::atomic<bool> m_Running;
-    std::mutex m_RunningLock;
 
 public:
     SingleShotTimer()
@@ -37,13 +42,14 @@ public:
         Stop();
     }
 
-    TimerInfo* ScheduleTask(unsigned long milli, std::function <void()> to, std::function <void()> cancel)
+    bool ScheduleTask(unsigned long milli, std::function <void()> to)
     {
-        std::lock_guard<std::mutex> lock(m_Lock);
+        std::lock_guard<std::mutex> APILock(m_APILock);
         if(m_Running == false)
         {
-            return nullptr;
+            return false;
         }
+        std::lock_guard<std::mutex> ActiveTimerInfoListLock(m_ActiveTimerInfoListLock);
         TimerInfo* newone = nullptr;
         unsigned long position = m_ActiveTimerInfoList.size();
         // 1. Get a free TimerInfo
@@ -53,15 +59,13 @@ public:
         }
         catch(const std::bad_alloc& ex)
         {
-            return nullptr;
+            return false;
         }
         // 2. Setup TimerInfo
-        newone->m_Active = true;
         newone->m_TargetTime = std::chrono::steady_clock::now() + std::chrono::milliseconds(milli);
         newone->m_TimeoutHandler = to;
-        newone->m_CancelHandler = cancel;
 
-        // 3. Push TimerInfo into ActiveTimerList
+        // 3. Push TimerInfo into ActiveTimerList, which is min heap.
         try
         {
             m_ActiveTimerInfoList.push_back(newone);
@@ -70,9 +74,8 @@ public:
         {
             std::cout<<ex.what()<<std::endl;
             delete newone;
-            return nullptr;
+            return false;
         }
-
         while(position)
         {
             if(m_ActiveTimerInfoList[position]->m_TargetTime < m_ActiveTimerInfoList[position/2]->m_TargetTime)
@@ -87,47 +90,30 @@ public:
             }
         }
         m_Condition.notify_one();
-        return newone;
-    }
-
-    void Cancel(TimerInfo* timer)
-    {
-        std::lock_guard<std::mutex> lock(m_Lock);
-        timer->m_Active = false;
-        m_ThreadPool.Enqueue([timer](){
-            if(timer->m_CancelHandler)
-            {
-                timer->m_CancelHandler();
-            }
-            /* timer will be deleted in m_Thread. */
-        });
+        return true;
     }
 
     void Start()
     {
-        std::unique_lock<std::mutex> lock(m_RunningLock);
-        std::cout<<"TIMERINFOPOOLING OFF"<<std::endl;
+        std::unique_lock<std::mutex> APILock(m_APILock);
+        if(m_Running)
         {
-            std::unique_lock<std::mutex> lock(m_Lock);
-            if(m_Running)
-            {
-                return;
-            }
+            return;
         }
+        m_Running = true;
         try
         {
             m_Thread = new std::thread([this]()
             {
-                m_Running = true;
                 while(m_Running)
                 {
                     TimerInfo* task = nullptr;
-                    TimerInfo* temp = nullptr;
                     {
-                        std::unique_lock<std::mutex> lock(m_Lock);
+                        std::unique_lock<std::mutex> ActiveTimerInfoListLock(m_ActiveTimerInfoListLock);
+#ifdef BUSYWAITING
                         while(m_ActiveTimerInfoList.size() == 0 && m_Running)
                         {
-                            m_Condition.wait(lock);
+                            m_Condition.wait(ActiveTimerInfoListLock);
                         }
                         if(m_Running == false)
                         {
@@ -141,77 +127,99 @@ public:
                             const unsigned long TIMERS = m_ActiveTimerInfoList.size();
                             // re-arrang heap
                             {
-                                unsigned long position = 0;
-                                while(position < TIMERS)
+                                unsigned long parent = 0;
+                                while(parent < TIMERS)
                                 {
-                                    if(position*2+2 < TIMERS)
+                                    const unsigned long leftchild = parent*2+1;
+                                    const unsigned long rightchild = parent*2+2;
+                                    unsigned long target = parent;
+                                    target = (leftchild < TIMERS && m_ActiveTimerInfoList[leftchild]->m_TargetTime < m_ActiveTimerInfoList[target]->m_TargetTime?
+                                                  leftchild:
+                                                  target);
+                                    target = (rightchild < TIMERS && m_ActiveTimerInfoList[rightchild]->m_TargetTime < m_ActiveTimerInfoList[target]->m_TargetTime?
+                                                  rightchild:
+                                                  target);
+                                    if(parent == target)
                                     {
-                                        temp = ( m_ActiveTimerInfoList[position]->m_TargetTime < m_ActiveTimerInfoList[position*2+1]->m_TargetTime?
-                                                    ( m_ActiveTimerInfoList[position]->m_TargetTime < m_ActiveTimerInfoList[position*2+2]->m_TargetTime ? m_ActiveTimerInfoList[position] : m_ActiveTimerInfoList[position*2+2]) :
-                                            ( m_ActiveTimerInfoList[position*2+1]->m_TargetTime < m_ActiveTimerInfoList[position*2+2]->m_TargetTime ? m_ActiveTimerInfoList[position*2+1] : m_ActiveTimerInfoList[position*2+2]));
-                                        if(temp == m_ActiveTimerInfoList[position])
-                                        {
-                                            break;
-                                        }
-                                        else if(temp == m_ActiveTimerInfoList[position*2+1])
-                                        {
-                                            m_ActiveTimerInfoList[position*2+1] = m_ActiveTimerInfoList[position];
-                                            m_ActiveTimerInfoList[position] = temp;
-                                            position = position*2+1;
-                                        }
-                                        else
-                                        {
-                                            m_ActiveTimerInfoList[position*2+2] = m_ActiveTimerInfoList[position];
-                                            m_ActiveTimerInfoList[position] = temp;
-                                            position = position*2+2;
-                                        }
-                                    }
-                                    else if(position*2+1 < TIMERS)
-                                    {
-                                        temp = ( m_ActiveTimerInfoList[position]->m_TargetTime < m_ActiveTimerInfoList[position*2+1]->m_TargetTime? m_ActiveTimerInfoList[position] : m_ActiveTimerInfoList[position*2+1]);
-                                        if(temp == m_ActiveTimerInfoList[position])
-                                        {
-                                            break;
-                                        }
-                                        else
-                                        {
-                                            m_ActiveTimerInfoList[position*2+1] = m_ActiveTimerInfoList[position];
-                                            m_ActiveTimerInfoList[position] = temp;
-                                            position = position*2+1;
-                                        }
+                                        break;
                                     }
                                     else
                                     {
-                                        break;
+                                        TimerInfo* task = m_ActiveTimerInfoList[target];
+                                        m_ActiveTimerInfoList[target] = m_ActiveTimerInfoList[parent];
+                                        m_ActiveTimerInfoList[parent] = task;
+                                        parent = target;
                                     }
                                 }
                             }
                         }
                         else
                         {
-                            // Yield CPU Resource to other threads
-                            std::this_thread::sleep_for (std::chrono::milliseconds(0));
+                            std::this_thread::sleep_for (std::chrono::seconds(0));
                             continue;
                         }
+#else
+                        if(m_ActiveTimerInfoList.size() == 0)
+                        {
+                            while(m_ActiveTimerInfoList.size() == 0 && m_Running)
+                            {
+                                m_Condition.wait(ActiveTimerInfoListLock);
+                            }
+                            if(!m_Running)
+                            {
+                                return;
+                            }
+                        }
+                        while(m_ActiveTimerInfoList[0]->m_TargetTime > std::chrono::steady_clock::now() && m_Running)
+                        {
+                            m_Condition.wait_for(ActiveTimerInfoListLock, m_ActiveTimerInfoList[0]->m_TargetTime - std::chrono::steady_clock::now());
+                        }
+                        if(m_Running == false)
+                        {
+                            return;
+                        }
+                        task = m_ActiveTimerInfoList[0];
+                        m_ActiveTimerInfoList[0] = m_ActiveTimerInfoList.back();
+                        m_ActiveTimerInfoList.pop_back();
+                        const unsigned long TIMERS = m_ActiveTimerInfoList.size();
+                        // re-arrang heap
+                        {
+                            unsigned long parent = 0;
+                            while(parent < TIMERS)
+                            {
+                                const unsigned long leftchild = parent*2+1;
+                                const unsigned long rightchild = parent*2+2;
+                                unsigned long target = parent;
+                                target = (leftchild < TIMERS && m_ActiveTimerInfoList[leftchild]->m_TargetTime < m_ActiveTimerInfoList[target]->m_TargetTime?
+                                              leftchild:
+                                              target);
+                                target = (rightchild < TIMERS && m_ActiveTimerInfoList[rightchild]->m_TargetTime < m_ActiveTimerInfoList[target]->m_TargetTime?
+                                              rightchild:
+                                              target);
+                                if(parent == target)
+                                {
+                                    break;
+                                }
+                                else
+                                {
+                                    TimerInfo* task = m_ActiveTimerInfoList[target];
+                                    m_ActiveTimerInfoList[target] = m_ActiveTimerInfoList[parent];
+                                    m_ActiveTimerInfoList[parent] = task;
+                                    parent = target;
+                                }
+                            }
+                        }
+#endif
                     }
                     if(task)
                     {
-                        if(task->m_Active)
-                        {
-                            m_ThreadPool.Enqueue([this, task](){
-                                if(task->m_TimeoutHandler)
-                                {
-                                    task->m_TimeoutHandler();
-                                }
-                                {
-                                    delete task;
-                                }
-                            });
-                        }
-                        else
-                        {
+                        m_ThreadPool.Enqueue([this, task](){
+                            if(task->m_TimeoutHandler)
+                            {
+                                task->m_TimeoutHandler();
+                            }
                             delete task;
-                        }
+                        });
                     }
                 }
             });
@@ -224,7 +232,7 @@ public:
 
     void Stop()
     {
-        std::unique_lock<std::mutex> lock(m_RunningLock);
+        std::unique_lock<std::mutex> APIlock(m_APILock);
         if(!m_Running)
         {
             return;
@@ -236,11 +244,14 @@ public:
             m_Thread->join();
         }
         delete m_Thread;
-        for(unsigned long i = 0 ; i < m_ActiveTimerInfoList.size() ; i++)
         {
-            delete m_ActiveTimerInfoList[i];
+            std::unique_lock<std::mutex> ActiveTimerInfoListLock(m_ActiveTimerInfoListLock);
+            for(unsigned long i = 0 ; i < m_ActiveTimerInfoList.size() ; i++)
+            {
+                delete m_ActiveTimerInfoList[i];
+            }
+            m_ActiveTimerInfoList.clear();
         }
-        m_ActiveTimerInfoList.clear();
     }
 };
 
--- singleshottimer/main.cpp
+++ singleshottimer/main.cpp
@@ -4,34 +4,35 @@
 
 int main()
 {
-    SingleShotTimer<10> timer;
+    SingleShotTimer<1> timer;
 
     std::thread StartStop = std::thread([&timer](){
         while(1)
         {
             std::cout<<"Stop\n";
             timer.Stop();
-            std::this_thread::sleep_for(std::chrono::seconds(1));
+            std::this_thread::sleep_for(std::chrono::milliseconds(20));
             std::cout<<"Start\n";
             timer.Start();
-            std::this_thread::sleep_for(std::chrono::seconds(1));
+            std::this_thread::sleep_for(std::chrono::milliseconds(20));
         }
     });
     StartStop.detach();
 
     while(1)
     {
-        timer.ScheduleTask(5, []{std::cout<<"TO 5000\n";}, nullptr);
-        timer.ScheduleTask(1, []{std::cout<<"TO 1000\n";}, nullptr);
-        timer.ScheduleTask(3, []{std::cout<<"TO 3000\n";}, nullptr);
-        timer.ScheduleTask(4, []{std::cout<<"TO 4000\n";}, nullptr);
-        timer.ScheduleTask(2, []{std::cout<<"TO 2000\n";}, nullptr);
-        timer.ScheduleTask(2, []{std::cout<<"TO 2500\n";}, nullptr);
-        timer.ScheduleTask(1, []{std::cout<<"TO 1500\n";}, nullptr);
-        timer.ScheduleTask(3, []{std::cout<<"TO 3500\n";}, nullptr);
-        timer.ScheduleTask(4, []{std::cout<<"TO 4500\n";}, nullptr);
-        timer.ScheduleTask(5, []{std::cout<<"TO 5500\n";}, nullptr);
+        timer.ScheduleTask(5, []{std::cout<<"TO 9\n";});
+        timer.ScheduleTask(1, []{std::cout<<"TO 1\n";});
+        timer.ScheduleTask(3, []{std::cout<<"TO 5\n";});
+        timer.ScheduleTask(4, []{std::cout<<"TO 7\n";});
+        timer.ScheduleTask(2, []{std::cout<<"TO 3\n";});
+        timer.ScheduleTask(2, []{std::cout<<"TO 4\n";});
+        timer.ScheduleTask(1, []{std::cout<<"TO 2\n";});
+        timer.ScheduleTask(3, []{std::cout<<"TO 6\n";});
+        timer.ScheduleTask(4, []{std::cout<<"TO 8\n";});
+        timer.ScheduleTask(5, []{std::cout<<"TO 10\n";});
         std::this_thread::sleep_for(std::chrono::milliseconds(10));
     }
+
     return 0;
 }

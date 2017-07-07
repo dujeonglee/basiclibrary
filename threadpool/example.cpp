f2fa6bf0798ab4f69ee796d99fba33ff56140422
Dujeong Lee <dujeong.lee82@gmail.com>
2017-07-07 11:41:11 +0900

    Update example.cpp

--- threadpool/example.cpp
+++ threadpool/example.cpp
@@ -1,34 +1,96 @@
-// condition_variable::wait (with predicate)
-#include <iostream>           // std::cout
+#include <unistd.h>
+#include <iostream>
+#include <cstdint>
+#include <string>
 #include "ThreadPool.h"
 
-int main ()
+#define MAXTASKS  (10)
+
+#define YIELD_CPU(x) std::this_thread::sleep_for(std::chrono::milliseconds(x))
+// Interesting example of serialized data access to shared memory space using ThreadPool with a single worker.
+class VeryImportantData
 {
-    ThreadPool<3, 3>* pool = new ThreadPool<3, 3>();
-    std::thread StopStartThread = std::thread([pool](){
+    enum ACCESS_PRIORITY: uint8_t
+    {
+        READ = 0, /*Urgent*/
+        WRITE,    /*Less urgent*/
+        NUM_OF_PRIORITY
+    };
+private:
+    uint32_t m_ImportantData;
+    ThreadPool<ACCESS_PRIORITY::NUM_OF_PRIORITY,1> m_AccessRequests;
+
+public:
+    void SetImportantData(const uint32_t value)
+    {
+        VeryImportantData* const self = this;
+        while(m_AccessRequests.Tasks() >= MAXTASKS){YIELD_CPU(0);}
+        m_AccessRequests.Enqueue([self, value](){
+            self->m_ImportantData = value;
+        }, ACCESS_PRIORITY::WRITE);
+    }
+
+    uint32_t GetImportantData()
+    {
+        VeryImportantData* const self = this;
+        volatile bool Ready = false;
+        uint32_t ret;
+
+        while(m_AccessRequests.Tasks() >= MAXTASKS){YIELD_CPU(0);}
+
+        m_AccessRequests.Enqueue([self, &Ready,&ret](){
+            ret = self->m_ImportantData;
+            Ready = true;
+        }, ACCESS_PRIORITY::READ);
+
+        while(!Ready){YIELD_CPU(0);}
+
+        return ret;
+    }
+};
+
+int main(int argc, char *argv[])
+{
+    VeryImportantData important;
+    VeryImportantData* const p_important = &important;
+    std::thread worker1;
+    std::thread worker2;
+    std::thread worker3;
+    std::thread worker4;
+
+    std::atomic<uint32_t> data;
+    data = 0;
+    worker1 = std::thread([&data, p_important](){
         while(1)
         {
-            pool->Stop();
-            std::this_thread::sleep_for(std::chrono::seconds(1));
-            pool->Start();
-            std::this_thread::sleep_for(std::chrono::seconds(1));
+            p_important->SetImportantData(data++);
         }
     });
-    StopStartThread.detach();
-    int a = 0;
-    int b = 0;
-    int c = 0;
-    std::thread EnqueueThread = std::thread([pool,&a,&b,&c](){
+    worker1.detach();
+    worker2 = std::thread([p_important](){
         while(1)
         {
-            pool->Enqueue([&a](){a++;}, 0);
-            pool->Enqueue([&b](){b++;}, 1);
-            pool->Enqueue([&c](){c++;}, 2);
+            std::cout<<p_important->GetImportantData()<<std::endl;
         }
     });
-    EnqueueThread.detach();
-    std::this_thread::sleep_for(std::chrono::seconds(10));
-    delete pool;
-    std::cout<<a<<":"<<b<<":"<<c<<std::endl;
-    return 0;
+    worker2.detach();
+    worker3 = std::thread([&data, p_important](){
+        while(1)
+        {
+            p_important->SetImportantData(data++);
+        }
+    });
+    worker3.detach();
+    worker4 = std::thread([p_important](){
+        while(1)
+        {
+            std::cout<<p_important->GetImportantData()<<std::endl;
+        }
+    });
+    worker4.detach();
+	while(1)
+	{
+		sleep(10);
+	}
+	return 0;
 }

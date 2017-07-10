b297d08d92bdbe32e020a2eb66fda510b31c0aa0
Dujeong Lee <dujeong.lee82@gmail.com>
2017-07-10 20:47:00 +0900

    Periodic task example

--- singleshottimer/main.cpp
+++ singleshottimer/main.cpp
@@ -2,41 +2,42 @@
 #include <iostream>
 #include <unistd.h>
 
+class Interval
+{
+private:
+    SingleShotTimer<2, 1> m_Timer;
+    std::atomic<bool> m_Running;
+    unsigned char i;
+public:
+    void PeriodicTask()
+    {
+        if(m_Running)
+        {
+            std::cout<<"Tick: "<<+i++<<std::endl;
+            Interval* const self = this;
+            m_Timer.ScheduleTask(1000, [self](){
+                self->PeriodicTask();
+            });
+        }
+    }
+    void Start()
+    {
+        i = 0;
+        m_Running = true;
+        PeriodicTask();
+    }
+    void Stop()
+    {
+        m_Running = false;
+    }
+};
+
 int main()
 {
-    SingleShotTimer<2, 1> timer;
-
-    std::thread StartStop = std::thread([&timer](){
-        while(1)
-        {
-            std::cout<<"Stop\n";
-            timer.Stop();
-            std::this_thread::sleep_for(std::chrono::milliseconds(20));
-            std::cout<<"Start\n";
-            timer.Start();
-            std::this_thread::sleep_for(std::chrono::milliseconds(20));
-        }
-    });
-    StartStop.detach();
-
-    while(1)
-    {
-        /*
-        timer.ScheduleTask(5, []{std::cout<<"TO 9\n";});
-        timer.ScheduleTask(1, []{std::cout<<"TO 1\n";});
-        timer.ScheduleTask(3, []{std::cout<<"TO 5\n";});
-        timer.ScheduleTask(4, []{std::cout<<"TO 7\n";});
-        timer.ScheduleTask(2, []{std::cout<<"TO 3\n";});
-        timer.ScheduleTask(2, []{std::cout<<"TO 4\n";});
-        timer.ScheduleTask(1, []{std::cout<<"TO 2\n";});
-        timer.ScheduleTask(3, []{std::cout<<"TO 6\n";});
-        timer.ScheduleTask(4, []{std::cout<<"TO 8\n";});
-        timer.ScheduleTask(5, []{std::cout<<"TO 10\n";});
-        */
-        timer.ScheduleTask(1, []{std::cout<<"TO Priority 1\n";}, 1);
-        timer.ScheduleTask(1, []{std::cout<<"TO Priority 0\n";}, 0);
-        std::this_thread::sleep_for(std::chrono::milliseconds(10));
-    }
-
+    Interval interval;
+    interval.Start();
+    std::this_thread::sleep_for(std::chrono::milliseconds(9500));
+    interval.Stop();
+    while(1);
     return 0;
 }

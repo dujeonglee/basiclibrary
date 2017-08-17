6a2f2b26c013f843cfed0a1ddfab52bfb0a3e614
Dujeong Lee <dujeong.lee82@gmail.com>
2017-08-18 08:16:02 +0900

    Update SingleShotTimer.h

--- singleshottimer/SingleShotTimer.h
+++ singleshottimer/SingleShotTimer.h
@@ -24,281 +24,286 @@ typedef std::chrono::time_point<CLOCK> SSTTime;
 class TimerInfo
 {
 public:
-    SSTTime m_TargetTime;
-    std::function <void()> m_TimeoutHandler;
-    uint32_t m_Priority;
-    uint32_t m_TimerID;
-    bool m_Active;
+	SSTTime m_TargetTime;
+	std::function <void()> m_TimeoutHandler;
+	uint32_t m_Priority;
+	uint32_t m_TimerID;
+	bool m_Active;
 };
 
 template <uint32_t PRIORITY, uint32_t CONCURRENCY>
 class SingleShotTimer
 {
 private:
-    std::mutex m_APILock;
+	std::mutex m_APILock;
 
-    std::mutex m_ActiveTimerInfoListLock;
-    std::condition_variable m_Condition;
-    std::vector<TimerInfo*> m_ActiveTimerInfoList;
-    std::thread* m_Thread;
-    ThreadPool<PRIORITY, CONCURRENCY> m_ThreadPool;
-    std::atomic<bool> m_Running;
-    uint32_t m_TimerID;
-    #define INVALID_TIMER_ID    ((uint32_t)0)
-    #define IMMEDIATE_TIMER_ID  ((uint32_t)1)
-    #define MINIMUM_TIMER_ID    ((uint32_t)2)
+	std::mutex m_ActiveTimerInfoListLock;
+	std::condition_variable m_Condition;
+	std::vector<TimerInfo*> m_ActiveTimerInfoList;
+	std::thread* m_Thread;
+	ThreadPool<PRIORITY, CONCURRENCY> m_ThreadPool;
+	std::atomic<bool> m_Running;
+	uint32_t m_TimerID;
+#define INVALID_TIMER_ID    ((uint32_t)0)
+#define IMMEDIATE_TIMER_ID  ((uint32_t)1)
+#define MINIMUM_TIMER_ID    ((uint32_t)2)
 public:
-    SingleShotTimer()
-    {
-        m_Running = false;
-        m_TimerID = MINIMUM_TIMER_ID;
-        Start();
-    }
+	SingleShotTimer()
+	{
+		m_Running = false;
+		m_TimerID = MINIMUM_TIMER_ID;
+		Start();
+	}
 
-    ~SingleShotTimer()
-    {
-        Stop();
-    }
+	~SingleShotTimer()
+	{
+		Stop();
+	}
 
-    uint32_t ImmediateTaskNoExcept(const std::function <void()> to, const uint32_t priority = 0)
-    {
-        uint32_t ret = INVALID_TIMER_ID;
-        while (ret == INVALID_TIMER_ID && m_Running)
-        {
-            ret = ImmediateTask(to, priority);
-        }
-        return ret;
-    }
+	uint32_t ImmediateTaskNoExcept(const std::function <void()> to, const uint32_t priority = 0)
+	{
+		uint32_t ret = INVALID_TIMER_ID;
+		while (ret == INVALID_TIMER_ID && m_Running)
+		{
+			ret = ImmediateTask(to, priority);
+		}
+		return ret;
+	}
 
-    uint32_t ScheduleTaskNoExcept(const uint32_t milli, const std::function <void()> to, const uint32_t priority = 0)
-    {
-        uint32_t ret = INVALID_TIMER_ID;
-        while (ret == INVALID_TIMER_ID && m_Running)
-        {
-            ret = ScheduleTask(milli, to, priority);
-        }
-        return ret;
-    }
-    
-    uint32_t ImmediateTask(const std::function <void()> to, const uint32_t priority = 0)
-    {
-        return ScheduleTask(0, to, priority);
-    }
+	uint32_t ScheduleTaskNoExcept(const uint32_t milli, const std::function <void()> to, const uint32_t priority = 0)
+	{
+		uint32_t ret = INVALID_TIMER_ID;
+		while (ret == INVALID_TIMER_ID && m_Running)
+		{
+			ret = ScheduleTask(milli, to, priority);
+		}
+		return ret;
+	}
 
-    uint32_t ScheduleTask(const uint32_t milli, const std::function <void()> to, const uint32_t priority = 0)
-    {
-        std::lock_guard<std::mutex> APILock(m_APILock);
-        if(m_Running == false)
-        {
-            return INVALID_TIMER_ID;
-        }
-        if(milli == 0)
-        {
-            return (m_ThreadPool.Enqueue([to](){to();}, priority) ? 
-                IMMEDIATE_TIMER_ID : 
-                INVALID_TIMER_ID); /*One cannot cancel immediate task*/
-        }
-        TimerInfo* newone = nullptr;
-        // 1. Get a free TimerInfo
-        try
-        {
-            newone = new TimerInfo();
-        }
-        catch(const std::bad_alloc& ex)
-        {
-            return INVALID_TIMER_ID;
-        }
-        // 2. Setup TimerInfo
-        newone->m_TargetTime = CLOCK::now() + std::chrono::milliseconds(milli);
-        newone->m_TimeoutHandler = to;
-        newone->m_Priority = priority;
-        const uint32_t TID = newone->m_TimerID = m_TimerID++;
-        if(m_TimerID < MINIMUM_TIMER_ID )
-        {
-            m_TimerID = MINIMUM_TIMER_ID;
-        }
-        newone->m_Active = true;
+	uint32_t ImmediateTask(const std::function <void()> to, const uint32_t priority = 0)
+	{
+		return ScheduleTask(0, to, priority);
+	}
 
-        // 3. Push TimerInfo into ActiveTimerList, which is min heap.
-        {
-            std::lock_guard<std::mutex> ActiveTimerInfoListLock(m_ActiveTimerInfoListLock);
-            try
-            {
-                m_ActiveTimerInfoList.push_back(newone);
-            }
-            catch(std::bad_alloc& ex)
-            {
-                std::cout<<ex.what()<<std::endl;
-                delete newone;
-                return INVALID_TIMER_ID;
-            }
-            std::push_heap(m_ActiveTimerInfoList.begin(), m_ActiveTimerInfoList.end(), [](TimerInfo* &a, TimerInfo* &b)->bool{
-                return a->m_TargetTime > b->m_TargetTime;
-            });
-        }
-        m_Condition.notify_one();
-        return TID;
-    }
+	uint32_t ScheduleTask(const uint32_t milli, const std::function <void()> to, const uint32_t priority = 0)
+	{
+		std::lock_guard<std::mutex> APILock(m_APILock);
+		if (m_Running == false)
+		{
+			return INVALID_TIMER_ID;
+		}
+		if (milli == 0)
+		{
+			return (m_ThreadPool.Enqueue([to]() {to(); }, priority) ?
+				IMMEDIATE_TIMER_ID :
+				INVALID_TIMER_ID); /*One cannot cancel immediate task*/
+		}
+		TimerInfo* newone = nullptr;
+		// 1. Get a free TimerInfo
+		try
+		{
+			newone = new TimerInfo();
+		}
+		catch (const std::bad_alloc& ex)
+		{
+			return INVALID_TIMER_ID;
+		}
+		// 2. Setup TimerInfo
+		newone->m_TargetTime = CLOCK::now() + std::chrono::milliseconds(milli);
+		newone->m_TimeoutHandler = to;
+		newone->m_Priority = priority;
+		const uint32_t TID = newone->m_TimerID = m_TimerID++;
+		if (m_TimerID < MINIMUM_TIMER_ID)
+		{
+			m_TimerID = MINIMUM_TIMER_ID;
+		}
+		newone->m_Active = true;
 
-    void CancelTask(const uint32_t timerid)
-    {
-        std::lock_guard<std::mutex> APILock(m_APILock);
-        if(m_Running == false)
-        {
-            return;
-        }
-        if(timerid < MINIMUM_TIMER_ID)
-        {
-            return;
-        }
-        std::lock_guard<std::mutex> ActiveTimerInfoListLock(m_ActiveTimerInfoListLock);
-        for(uint32_t i = 0 ; i < m_ActiveTimerInfoList.size() ; i++)
-        {
-            if(m_ActiveTimerInfoList[i]->m_TimerID == timerid)
-            {
-                m_ActiveTimerInfoList[i]->m_Active = false;
-                return;
-            }
-        }
-    }
+		// 3. Push TimerInfo into ActiveTimerList, which is min heap.
+		{
+			std::lock_guard<std::mutex> ActiveTimerInfoListLock(m_ActiveTimerInfoListLock);
+			try
+			{
+				m_ActiveTimerInfoList.push_back(newone);
+			}
+			catch (std::bad_alloc& ex)
+			{
+				std::cout << ex.what() << std::endl;
+				delete newone;
+				return INVALID_TIMER_ID;
+			}
+			std::push_heap(m_ActiveTimerInfoList.begin(), m_ActiveTimerInfoList.end(), [](TimerInfo* &a, TimerInfo* &b)->bool {
+				return a->m_TargetTime > b->m_TargetTime;
+			});
+		}
+		m_Condition.notify_one();
+		return TID;
+	}
 
-    void Start()
-    {
-        std::unique_lock<std::mutex> APILock(m_APILock);
-        if(m_Running)
-        {
-            return;
-        }
-        m_Running = true;
-        try
-        {
-            m_ThreadPool.Start();
-            SingleShotTimer* const self = this;
-            m_Thread = new std::thread([self]()
-            {
-                while(self->m_Running)
-                {
-                    TimerInfo* task = nullptr;
-                    {
-                        std::unique_lock<std::mutex> ActiveTimerInfoListLock(self->m_ActiveTimerInfoListLock);
-                        if(self->m_ActiveTimerInfoList.size() == 0)
-                        {
-                            while(self->m_ActiveTimerInfoList.size() == 0 && self->m_Running)
-                            {
-                                self->m_Condition.wait_for(ActiveTimerInfoListLock, std::chrono::milliseconds(500));
-                            }
-                        }
-                        if(!self->m_Running)
-                        {
-                            return;
-                        }
+	void CancelTask(const uint32_t timerid)
+	{
+		std::lock_guard<std::mutex> APILock(m_APILock);
+		if (m_Running == false)
+		{
+			return;
+		}
+		if (timerid < MINIMUM_TIMER_ID)
+		{
+			return;
+		}
+		std::lock_guard<std::mutex> ActiveTimerInfoListLock(m_ActiveTimerInfoListLock);
+		for (uint32_t i = 0; i < m_ActiveTimerInfoList.size(); i++)
+		{
+			if (m_ActiveTimerInfoList[i]->m_TimerID == timerid)
+			{
+				m_ActiveTimerInfoList[i]->m_Active = false;
+				return;
+			}
+		}
+	}
+
+	void Start()
+	{
+		std::unique_lock<std::mutex> APILock(m_APILock);
+		if (m_Running)
+		{
+			return;
+		}
+		m_Running = true;
+		try
+		{
+			m_ThreadPool.Start();
+			SingleShotTimer* const self = this;
+			m_Thread = new std::thread([self]()
+			{
+				while (self->m_Running)
+				{
+					TimerInfo* task = nullptr;
+					{
+						std::unique_lock<std::mutex> ActiveTimerInfoListLock(self->m_ActiveTimerInfoListLock);
+						if (self->m_ActiveTimerInfoList.size() == 0)
+						{
+							while (self->m_ActiveTimerInfoList.size() == 0 && self->m_Running)
+							{
+								self->m_Condition.wait_for(ActiveTimerInfoListLock, std::chrono::milliseconds(500));
+							}
+						}
+						if (!self->m_Running)
+						{
+							return;
+						}
 #ifdef BUSYWAITING
-                        if(self->m_ActiveTimerInfoList[0]->m_TargetTime <= CLOCK::now() && self->m_Running)
-                        {
-                            std::pop_heap(self->m_ActiveTimerInfoList.begin(), self->m_ActiveTimerInfoList.end(), [](TimerInfo* &a, TimerInfo* &b)->bool{
-                                return a->m_TargetTime > b->m_TargetTime;
-                            });
-                            task = self->m_ActiveTimerInfoList.back();
-                            self->m_ActiveTimerInfoList.pop_back();
-                        }
-                        else
-                        {
-                            if(self->m_Running == false)
-                            {
-                                return;
-                            }
-                            std::this_thread::sleep_for (std::chrono::microseconds(100));
-                            continue;
-                        }
+						if (self->m_ActiveTimerInfoList[0]->m_TargetTime <= CLOCK::now() && self->m_Running)
+						{
+							std::pop_heap(self->m_ActiveTimerInfoList.begin(), self->m_ActiveTimerInfoList.end(), [](TimerInfo* &a, TimerInfo* &b)->bool {
+								return a->m_TargetTime > b->m_TargetTime;
+							});
+							task = self->m_ActiveTimerInfoList.back();
+							self->m_ActiveTimerInfoList.pop_back();
+						}
+						else
+						{
+							if (self->m_Running == false)
+							{
+								return;
+							}
+							std::this_thread::sleep_for(std::chrono::microseconds(100));
+							continue;
+						}
 #else
-                        while(self->m_ActiveTimerInfoList[0]->m_TargetTime > CLOCK::now() && self->m_Running)
-                        {
-                            self->m_Condition.wait_for(ActiveTimerInfoListLock, self->m_ActiveTimerInfoList[0]->m_TargetTime - CLOCK::now());
-                        }
-                        if(self->m_Running == false)
-                        {
-                            return;
-                        }
-                        std::pop_heap(self->m_ActiveTimerInfoList.begin(), self->m_ActiveTimerInfoList.end(), [](TimerInfo* &a, TimerInfo* &b)->bool{
-                            return a->m_TargetTime > b->m_TargetTime;
-                        });
-                        task = self->m_ActiveTimerInfoList.back();
-                        self->m_ActiveTimerInfoList.pop_back();
+						while (self->m_ActiveTimerInfoList[0]->m_TargetTime > CLOCK::now() && self->m_Running)
+						{
+							self->m_Condition.wait_for(ActiveTimerInfoListLock, self->m_ActiveTimerInfoList[0]->m_TargetTime - CLOCK::now());
+						}
+						if (self->m_Running == false)
+						{
+							return;
+						}
+						std::pop_heap(self->m_ActiveTimerInfoList.begin(), self->m_ActiveTimerInfoList.end(), [](TimerInfo* &a, TimerInfo* &b)->bool {
+							return a->m_TargetTime > b->m_TargetTime;
+						});
+						task = self->m_ActiveTimerInfoList.back();
+						self->m_ActiveTimerInfoList.pop_back();
 #endif
-                    }
-                    if(task && task->m_Active)
-                    {
-                        self->m_ThreadPool.Enqueue([task](){
-                            if(task->m_TimeoutHandler)
-                            {
-                                task->m_TimeoutHandler();
-                            }
-                            delete task;
-                        }, task->m_Priority);
-                    }
-                }
-            });
-        }
-        catch(const std::bad_alloc& ex)
-        {
-            std::cout<<ex.what()<<std::endl;
-        }
-    }
+					}
+					if (task && task->m_Active)
+					{
+						self->m_ThreadPool.Enqueue([task]() {
+							if (task->m_TimeoutHandler)
+							{
+								task->m_TimeoutHandler();
+							}
+							delete task;
+						}, task->m_Priority);
+					}
+				}
+			});
+		}
+		catch (const std::bad_alloc& ex)
+		{
+			std::cout << ex.what() << std::endl;
+		}
+	}
 
-    void Stop()
-    {
-        std::unique_lock<std::mutex> APIlock(m_APILock);
-        if(!m_Running)
-        {
-            return;
-        }
+	void Stop()
+	{
+		std::unique_lock<std::mutex> APIlock(m_APILock);
+		if (!m_Running)
+		{
+			return;
+		}
 
-        m_Running = false;
-        m_Condition.notify_one();
-        if(m_Thread->joinable())
-        {
-            m_Thread->join();
-        }
-        delete m_Thread;
-        {
-            std::unique_lock<std::mutex> ActiveTimerInfoListLock(m_ActiveTimerInfoListLock);
-            for(uint32_t i = 0 ; i < m_ActiveTimerInfoList.size() ; i++)
-            {
-                delete m_ActiveTimerInfoList[i];
-            }
-            m_ActiveTimerInfoList.clear();
-        }
-        m_ThreadPool.Stop();
-    }
+		m_Running = false;
+		m_Condition.notify_one();
+		if (m_Thread->joinable())
+		{
+			m_Thread->join();
+		}
+		delete m_Thread;
+		{
+			std::unique_lock<std::mutex> ActiveTimerInfoListLock(m_ActiveTimerInfoListLock);
+			for (uint32_t i = 0; i < m_ActiveTimerInfoList.size(); i++)
+			{
+				delete m_ActiveTimerInfoList[i];
+			}
+			m_ActiveTimerInfoList.clear();
+		}
+		m_ThreadPool.Stop();
+	}
 
-    size_t Timers()
-    {
-        std::unique_lock<std::mutex> APIlock(m_APILock);
-        std::unique_lock<std::mutex> ActiveTimerInfoListLock(m_ActiveTimerInfoListLock);
-        return m_ActiveTimerInfoList.size();
-    }
+	size_t Timers()
+	{
+		std::unique_lock<std::mutex> APIlock(m_APILock);
+		std::unique_lock<std::mutex> ActiveTimerInfoListLock(m_ActiveTimerInfoListLock);
+		return m_ActiveTimerInfoList.size();
+	}
 
-    bool Running()
-    {
-        std::unique_lock<std::mutex> APIlock(m_APILock);
-        return m_Running;
-    }
+	bool Running()
+	{
+		std::unique_lock<std::mutex> APIlock(m_APILock);
+		return m_Running;
+	}
 
 private:
-    static void PeriodicTaskWrapper(SingleShotTimer<PRIORITY, CONCURRENCY>* const timer, const uint32_t interval, const std::function<bool()> task, const uint32_t priority = 0)
-    {
-        std::function<void()> wrapper = std::bind(SingleShotTimer<PRIORITY, CONCURRENCY>::PeriodicTaskWrapper, timer, interval, task, priority);
-        if(task())
-        {
-            timer->ScheduleTaskNoExcept(interval, wrapper, priority);
-        }
-    }
+	static void PeriodicTaskWrapper(SingleShotTimer<PRIORITY, CONCURRENCY>* const timer, const uint32_t interval, const std::function<bool()> task, const uint32_t priority = 0)
+	{
+		std::function<void()> wrapper = std::bind(SingleShotTimer<PRIORITY, CONCURRENCY>::PeriodicTaskWrapper, timer, interval, task, priority);
+		if (task())
+		{
+			timer->ScheduleTaskNoExcept(interval, wrapper, priority);
+		}
+	}
 
 public:
-    void PeriodicTask(const uint32_t interval, const std::function<bool()> task, const uint32_t priority = 0)
-    {
-        SingleShotTimer<PRIORITY, CONCURRENCY>::PeriodicTaskWrapper(this, interval, task, priority);
-    }
+	void PeriodicTask(const uint32_t interval, const std::function<bool()> task, const uint32_t priority = 0)
+	{
+		SingleShotTimer<PRIORITY, CONCURRENCY>::PeriodicTaskWrapper(this, interval, task, priority);
+	}
 };
+
+// Alias.
+template <uint32_t PRIORITY, uint32_t CUNCURRENCY>
+using TaskScheduler = SingleShotTimer<PRIORITY, CUNCURRENCY>;
+
 #undef GCC_VERSION
 #endif

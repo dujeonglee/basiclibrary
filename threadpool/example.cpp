9d74f6ef84152354c703c54e47601449b1e51646
Dujeong Lee <dujeong.lee82@gmail.com>
2017-08-26 23:25:55 +0900

    Update example.cpp

--- threadpool/example.cpp
+++ threadpool/example.cpp
@@ -43,5 +43,23 @@ int main(int argc, char *argv[])
         while(threadpool.Tasks());
     }
     std::cout<<"--------------------------------------------------------------------"<<std::endl;
-	return 0;
+    {
+        std::cout<<"Three threads"<<std::endl;
+        ThreadPool<1/*Number of priority levels*/,3/*Number of threads*/> threadpool;
+        for(unsigned int i = 0 ; i < 10 ; i++)
+        {
+            threadpool.Enqueue([i, &threadpool](){
+                std::cout<<"Task "<<i<<std::endl;
+                std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+                if(i == 9)
+		{
+                    //threadpool.Stop(); // This leads to blocking process.
+                    threadpool.StopAsync(); // Instead, one must use StopAsync inside the task.
+		}
+            });
+        }
+        while(threadpool.Tasks());
+    }
+    std::cout<<"--------------------------------------------------------------------"<<std::endl;
+    return 0;
 }

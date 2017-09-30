8268b75864bb461e84353a101c0f6c2996cc978f
Dujeong Lee <dujeong.lee82@gmail.com>
2017-09-30 14:19:58 +0900

    remove m_WorkeInStop
    m_WorkerInStop can be local

--- threadpool/example.cpp
+++ threadpool/example.cpp
@@ -3,6 +3,7 @@
 
 int main(int argc, char *argv[])
 {
+    #if 0
     std::cout<<"--------------------------------------------------------------------"<<std::endl;
     {
         std::cout<<"One thread"<<std::endl;
@@ -42,6 +43,7 @@ int main(int argc, char *argv[])
         }
         while(threadpool.Tasks());
     }
+    #endif
     std::cout << "--------------------------------------------------------------------" << std::endl;
     {
         std::cout << "Stop" << std::endl;

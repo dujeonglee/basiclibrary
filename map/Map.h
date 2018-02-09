16fab9bbc076afc67194656ee0de80e9d18f5c58
Dujeong Lee <dujeong.lee82@gmail.com>
2018-02-09 15:55:25 +0900

    robinhood hasing has been implemented.

--- map/Map.h
+++ map/Map.h
@@ -2,6 +2,8 @@
 #include <functional> // std::hash
 #include <vector>     // std::vector
 
+namespace map
+{
 template <typename TYPE>
 class GenericObject
 {
@@ -50,6 +52,8 @@ public:
   }
 };
 
+namespace plain
+{
 template <typename KEY, typename DATA>
 class UnorderedMap
 {
@@ -157,13 +161,10 @@ public:
         m_Table[position].second.Get() = data.second;
         break;
       }
-      else
+      position++;
+      if (position == m_TableSize)
       {
-        position++;
-        if (position == m_TableSize)
-        {
-          position = 0;
-        }
+        position = 0;
       }
     } while (true);
     ReHash();
@@ -196,13 +197,10 @@ public:
         m_Table[position].second.Get() = std::move(data.second);
         break;
       }
-      else
+      position++;
+      if (position == m_TableSize)
       {
-        position++;
-        if (position == m_TableSize)
-        {
-          position = 0;
-        }
+        position = 0;
       }
     } while (true);
     ReHash();
@@ -272,24 +270,21 @@ public:
         m_Table[position].first.Get() = c_EmptyKey;
 
         // rehash entries
-        position++;
-        if (position == m_TableSize)
+        for (uint32_t old_position = (position + 1 == m_TableSize ? 0 : position + 1);
+             m_Table[old_position].first.Get() != c_EmptyKey;
+             old_position = (old_position + 1 == m_TableSize ? 0 : old_position + 1))
         {
-          position = 0;
-        }
-        while (m_Table[position].first.Get() != c_EmptyKey)
-        {
-          uint32_t new_position = (std::hash<KEY>{}(m_Table[position].first.Get()) % m_TableSize);
+          uint32_t new_position = (std::hash<KEY>{}(m_Table[old_position].first.Get()) % m_TableSize);
           do
           {
             if (m_Table[new_position].first.Get() == c_EmptyKey)
             {
-              m_Table[new_position].first.Get() = m_Table[position].first.Get();
-              m_Table[new_position].second.Get() = m_Table[position].second.Get();
-              m_Table[position].first.Get() = c_EmptyKey;
+              m_Table[new_position].first.Get() = m_Table[old_position].first.Get();
+              m_Table[new_position].second.Get() = m_Table[old_position].second.Get();
+              m_Table[old_position].first.Get() = c_EmptyKey;
               break;
             }
-            else if (new_position == position)
+            else if (new_position == old_position)
             {
               break;
             }
@@ -302,11 +297,6 @@ public:
               }
             }
           } while (true);
-          position++;
-          if (position == m_TableSize)
-          {
-            position = 0;
-          }
         }
         m_Elements--;
         return true;
@@ -387,3 +377,383 @@ public:
     return m_Elements;
   }
 };
+}
+
+namespace robinhood
+{
+template <typename KEY, typename DATA>
+class UnorderedMap
+{
+private:
+  std::vector<std::tuple<GenericObject<KEY>, GenericObject<DATA>, uint32_t>> m_Table;
+  uint32_t m_TableSize;
+  uint32_t m_Elements;
+  const KEY c_EmptyKey;
+  const DATA c_EmptyData;
+  DATA m_EmptyData;
+
+private:
+  void ReHash()
+  {
+    if (m_Elements <= m_TableSize * 2 / 3)
+    {
+      return;
+    }
+    if (m_TableSize == 0xffffffff)
+    {
+      return;
+    }
+    m_TableSize *= 2;
+    try
+    {
+      m_Table.resize(m_TableSize, std::tuple<GenericObject<KEY>, GenericObject<DATA>, uint32_t>(GenericObject<KEY>(c_EmptyKey), GenericObject<DATA>(c_EmptyData), 0));
+    }
+    catch (const std::exception &ex)
+    {
+      m_TableSize /= 2;
+      return;
+    }
+    // ReHash
+    for (uint32_t old_position = 0; old_position < m_TableSize; old_position++)
+    {
+      uint32_t position = std::hash<KEY>{}(std::get<0>(m_Table[old_position]).Get()) % m_TableSize;
+      std::tuple<GenericObject<KEY>, GenericObject<DATA>, uint32_t> item =
+          std::tuple<GenericObject<KEY>, GenericObject<DATA>, uint32_t>(GenericObject<KEY>(std::get<0>(m_Table[old_position]).Get()), GenericObject<DATA>(std::get<1>(m_Table[old_position]).Get()), 0);
+      std::get<0>(m_Table[old_position]).Get() = c_EmptyKey;
+      do
+      {
+        // Item with new key.
+        if (std::get<0>(m_Table[position]).Get() == c_EmptyKey)
+        {
+          std::get<0>(m_Table[position]).Get() = std::get<0>(item).Get();
+          std::get<1>(m_Table[position]).Get() = std::get<1>(item).Get();
+          std::get<2>(m_Table[position]) = std::get<2>(item);
+          break;
+        }
+        // Existing item.
+        else if (std::get<0>(m_Table[position]).Get() == std::get<0>(item).Get())
+        {
+          std::get<0>(m_Table[position]).Get() = std::get<0>(item).Get();
+          std::get<1>(m_Table[position]).Get() = std::get<1>(item).Get();
+          std::get<2>(m_Table[position]) = std::get<2>(item);
+          break;
+        }
+        // Robinhood
+        else if (std::get<2>(m_Table[position]) < std::get<2>(item))
+        {
+          std::swap(std::get<0>(m_Table[position]).Get(), std::get<0>(item).Get());
+          std::swap(std::get<1>(m_Table[position]).Get(), std::get<1>(item).Get());
+          std::swap(std::get<2>(m_Table[position]), std::get<2>(item));
+        }
+        position++;
+        if (position == m_TableSize)
+        {
+          position = 0;
+        }
+        std::get<2>(item)++;
+      } while (true);
+    }
+  }
+
+public:
+  UnorderedMap<KEY, DATA>() = delete;
+  UnorderedMap<KEY, DATA>(const KEY &emptykey, const DATA &emptydata) : c_EmptyKey(emptykey), c_EmptyData(emptydata)
+  {
+    m_Elements = 0;
+    m_TableSize = 0x1 << 16;
+    m_Table.resize(m_TableSize, std::tuple<GenericObject<KEY>, GenericObject<DATA>, uint32_t>(GenericObject<KEY>(c_EmptyKey), GenericObject<DATA>(c_EmptyData), 0));
+  }
+  UnorderedMap<KEY, DATA>(const UnorderedMap<KEY, DATA> &other)
+  {
+    c_EmptyKey = other.c_EmptyKey;
+    m_TableSize = other.m_TableSize;
+    m_Elements = other.m_Elements;
+    m_Table = other.m_Table;
+  }
+
+  UnorderedMap<KEY, DATA> &operator=(const UnorderedMap<KEY, DATA> &other)
+  {
+    return UnorderedMap<KEY, DATA>(other);
+  }
+
+  bool Insert(const std::pair<KEY, DATA> &data)
+  {
+    // Table is full.
+    if (m_Elements == m_TableSize)
+    {
+      return false;
+    }
+    // Get insert position.
+    uint32_t position = std::hash<KEY>{}(data.first) % m_TableSize;
+    std::tuple<GenericObject<KEY>, GenericObject<DATA>, uint32_t> item =
+        std::tuple<GenericObject<KEY>, GenericObject<DATA>, uint32_t>(GenericObject<KEY>(data.first), GenericObject<KEY>(data.second), 0);
+    do
+    {
+      // Item with new key.
+      if (std::get<0>(m_Table[position]).Get() == c_EmptyKey)
+      {
+        std::get<0>(m_Table[position]).Get() = std::get<0>(item).Get();
+        std::get<1>(m_Table[position]).Get() = std::get<1>(item).Get();
+        std::get<2>(m_Table[position]) = std::get<2>(item);
+        m_Elements++;
+        break;
+      }
+      // Existing item.
+      else if (std::get<0>(m_Table[position]).Get() == std::get<0>(item).Get())
+      {
+        std::get<0>(m_Table[position]).Get() = std::get<0>(item).Get();
+        std::get<1>(m_Table[position]).Get() = std::get<1>(item).Get();
+        std::get<2>(m_Table[position]) = std::get<2>(item);
+        break;
+      }
+      // Robinhood
+      else if (std::get<2>(m_Table[position]) < std::get<2>(item))
+      {
+        std::swap(std::get<0>(m_Table[position]).Get(), std::get<0>(item).Get());
+        std::swap(std::get<1>(m_Table[position]).Get(), std::get<1>(item).Get());
+        std::swap(std::get<2>(m_Table[position]), std::get<2>(item));
+      }
+      position++;
+      if (position == m_TableSize)
+      {
+        position = 0;
+      }
+      std::get<2>(item)++;
+    } while (true);
+    ReHash();
+    return true;
+  }
+
+  bool Insert(std::pair<KEY, DATA> &&data)
+  {
+    // Table is full.
+    if (m_Elements == m_TableSize)
+    {
+      return false;
+    }
+    // Get insert position.
+    uint32_t position = std::hash<KEY>{}(data.first) % m_TableSize;
+    std::tuple<GenericObject<KEY>, GenericObject<DATA>, uint32_t> item =
+        std::tuple<GenericObject<KEY>, GenericObject<DATA>, uint32_t>(GenericObject<KEY>(std::move(data.first)), GenericObject<DATA>(std::move(data.second)), 0);
+    do
+    {
+      // Item with new key.
+      if (std::get<0>(m_Table[position]).Get() == c_EmptyKey)
+      {
+        std::get<0>(m_Table[position]).Get() = std::get<0>(item).Get();
+        std::get<1>(m_Table[position]).Get() = std::get<1>(item).Get();
+        std::get<2>(m_Table[position]) = std::get<2>(item);
+        m_Elements++;
+        break;
+      }
+      // Existing item.
+      else if (std::get<0>(m_Table[position]).Get() == std::get<0>(item).Get())
+      {
+        std::get<0>(m_Table[position]).Get() = std::get<0>(item).Get();
+        std::get<1>(m_Table[position]).Get() = std::get<1>(item).Get();
+        std::get<2>(m_Table[position]) = std::get<2>(item);
+        break;
+      }
+      // Robinhood
+      else if (std::get<2>(m_Table[position]) < std::get<2>(item))
+      {
+        std::swap(std::get<0>(m_Table[position]).Get(), std::get<0>(item).Get());
+        std::swap(std::get<1>(m_Table[position]).Get(), std::get<1>(item).Get());
+        std::swap(std::get<2>(m_Table[position]), std::get<2>(item));
+      }
+      position++;
+      if (position == m_TableSize)
+      {
+        position = 0;
+      }
+      std::get<2>(item)++;
+    } while (true);
+    ReHash();
+    return true;
+  }
+
+  DATA *FindPtr(const KEY &key)
+  {
+    uint32_t position = std::hash<KEY>{}(key) % m_TableSize;
+    DATA *ret = nullptr;
+    do
+    {
+      if (std::get<0>(m_Table[position]).Get() == key)
+      {
+        ret = &(std::get<1>(m_Table[position]).Get());
+        break;
+      }
+      else if (std::get<0>(m_Table[position]).Get() == c_EmptyKey)
+      {
+        break;
+      }
+      else
+      {
+        position++;
+        if (position == m_TableSize)
+        {
+          position = 0;
+        }
+      }
+    } while (true);
+    return ret;
+  }
+
+  DATA &FindRef(const KEY &key)
+  {
+    uint32_t position = std::hash<KEY>{}(key) % m_TableSize;
+    do
+    {
+      if (std::get<0>(m_Table[position]).Get() == key)
+      {
+        return (std::get<1>(m_Table[position]).Get());
+      }
+      else if (std::get<0>(m_Table[position]).Get() == c_EmptyKey)
+      {
+        m_EmptyData = c_EmptyData;
+        return m_EmptyData;
+      }
+      else
+      {
+        position++;
+        if (position == m_TableSize)
+        {
+          position = 0;
+        }
+      }
+    } while (true);
+  }
+
+  bool Erase(const KEY &key)
+  {
+    uint32_t position = std::hash<KEY>{}(key) % m_TableSize;
+    do
+    {
+      if (std::get<0>(m_Table[position]).Get() == key)
+      {
+        // remove entry.
+        std::get<0>(m_Table[position]).Get() = c_EmptyKey;
+
+        // rehash entries
+        for (uint32_t old_position = (position + 1 == m_TableSize ? 0 : position + 1);
+             std::get<0>(m_Table[old_position]).Get() != c_EmptyKey;
+             old_position = (old_position + 1 == m_TableSize ? 0 : old_position + 1))
+        {
+          uint32_t new_position = std::hash<KEY>{}(std::get<0>(m_Table[old_position]).Get()) % m_TableSize;
+          std::tuple<GenericObject<KEY>, GenericObject<DATA>, uint32_t> item =
+              std::tuple<GenericObject<KEY>, GenericObject<DATA>, uint32_t>(GenericObject<KEY>(std::get<0>(m_Table[old_position]).Get()), GenericObject<DATA>(std::get<1>(m_Table[old_position]).Get()), 0);
+          std::get<0>(m_Table[old_position]).Get() = c_EmptyKey;
+          do
+          {
+            // Item with new key.
+            if (std::get<0>(m_Table[new_position]).Get() == c_EmptyKey)
+            {
+              std::get<0>(m_Table[new_position]).Get() = std::get<0>(item).Get();
+              std::get<1>(m_Table[new_position]).Get() = std::get<1>(item).Get();
+              std::get<2>(m_Table[new_position]) = std::get<2>(item);
+              break;
+            }
+            // Existing item.
+            else if (std::get<0>(m_Table[new_position]).Get() == std::get<0>(item).Get())
+            {
+              std::get<0>(m_Table[new_position]).Get() = std::get<0>(item).Get();
+              std::get<1>(m_Table[new_position]).Get() = std::get<1>(item).Get();
+              std::get<2>(m_Table[new_position]) = std::get<2>(item);
+              break;
+            }
+            // Robinhood
+            else if (std::get<2>(m_Table[new_position]) < std::get<2>(item))
+            {
+              std::swap(std::get<0>(m_Table[new_position]).Get(), std::get<0>(item).Get());
+              std::swap(std::get<1>(m_Table[new_position]).Get(), std::get<1>(item).Get());
+              std::swap(std::get<2>(m_Table[new_position]), std::get<2>(item));
+            }
+            new_position++;
+            if (new_position == m_TableSize)
+            {
+              new_position = 0;
+            }
+            std::get<2>(item)++;
+          } while (true);
+        }
+        m_Elements--;
+        return true;
+      }
+      else if (std::get<0>(m_Table[position]).Get() == c_EmptyKey)
+      {
+        return false;
+      }
+      else
+      {
+        position++;
+        if (position == m_TableSize)
+        {
+          position = 0;
+        }
+      }
+    } while (true);
+  }
+
+  class Iterator
+  {
+  private:
+    UnorderedMap<KEY, DATA> *m_List;
+    uint32_t m_Position;
+
+  public:
+    Iterator(UnorderedMap<KEY, DATA> *list, uint32_t position) : m_List(list), m_Position(position) {}
+    Iterator(const Iterator &it) : m_List(it.m_List), m_Position(it.m_Position) {}
+    Iterator &operator++()
+    {
+      do
+      {
+        m_Position++;
+        if (std::get<0>(m_List->m_Table[m_Position]).Get() != m_List->c_EmptyKey)
+        {
+          break;
+        }
+      } while (m_Position < m_List->m_Table.size());
+      return *this;
+    }
+    Iterator operator++(int)
+    {
+      Iterator tmp(*this);
+      operator++();
+      return tmp;
+    }
+    bool operator==(const Iterator &rhs) const
+    {
+      return (m_List == rhs.m_List) && (m_Position == rhs.m_Position);
+    }
+    bool operator!=(const Iterator &rhs) const
+    {
+      return (m_List != rhs.m_List) || (m_Position != rhs.m_Position);
+    }
+    DATA &operator*()
+    {
+      return std::get<1>(m_List->m_Table[m_Position]).Get();
+    }
+  };
+
+  Iterator begin()
+  {
+    uint32_t start = 0;
+    while (std::get<0>(m_Table[start]).Get() == c_EmptyKey)
+    {
+      start++;
+    }
+    return Iterator(this, start);
+  }
+
+  Iterator end()
+  {
+    return Iterator(this, m_TableSize);
+  }
+
+  const uint32_t Size()
+  {
+    return m_Elements;
+  }
+};
+}
+}
